2b20378bdba44aeb2bae5e32d93166d85643da11
long <long@ichunt.com>
2021-02-25 18:51:23 +0800

    init

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	xlog "github.com/ichunt2019/lxLog/log"
 	"time"
 )
@@ -17,7 +19,18 @@ func main(){
 	xlog.Instance("spu").Warn("111111111111111111111")
 	xlog.Instance("spu").Error("111111111111111111111")
 
+	err := test()
+	if err != nil{
+		xlog.Instance("sku").Info(fmt.Sprintf("%s",err))
+	}
 
 	time.Sleep(100*time.Second)
 
+}
+
+
+
+func test() (err error){
+	xlog.Instance("sku").Info("666")
+	return errors.New("错误")
 }
